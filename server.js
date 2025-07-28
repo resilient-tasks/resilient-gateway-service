67fc9cf9af97dcb620de9314939820b1f9f83a16
fmarsico8 <fmarsico@frba.utn.edu.ar>
2025-07-28 14:55:13 -0300

    feat: add user and auth services

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const path = require('path');
+const dotenv = require('dotenv');
 const gateway = require('express-gateway');
+
+dotenv.config();
 
 gateway()
   .load(path.join(__dirname, 'config'))
